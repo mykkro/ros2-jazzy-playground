3464a36edc18f50e5b264773ae90002052bf4038
Miroslav Uller <Miroslav.Uller@cvut.cz>
2025-02-12 15:48:17 +0100

    Added working setup for jazzy with RViz2, rqt and py-trees.

--- scripts/attach_terminal.ps1
+++
@@ -1,5 +0,0 @@
-# Get the container ID
-$CONTAINER = docker container ls | Where-Object { $_ -match "robotics-content-lab" } | ForEach-Object { $_.Split()[0] }
-
-# Execute the entrypoint script inside the container
-docker container exec -it $CONTAINER /entrypoint.sh tmux
